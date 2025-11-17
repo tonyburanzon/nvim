e978e269fbf649c4e456137664f0b3fa9295a284
Tony Buranzon <tony@buranzon.com>
2025-11-16 22:32:20 -0800

    More netrw options. Mainly the file format. Switching to tree view. Going to try it out for a while. See what happens.

--- lua/tony/set.lua
+++ lua/tony/set.lua
@@ -28,6 +28,12 @@ vim.opt.signcolumn = 'yes'
 
 vim.opt.mouse = ""
 
+-- Netrw things
+-- Open files in current window/ all one window.
 vim.g.netrw_browse_split = 0
+-- Set netrw to tree listing style by default
+vim.g.netrw_liststyle = 3
+-- Remove the banner at the top
 vim.g.netrw_banner = 0
+-- Set the width of the netrw window
 vim.g.netrw_winsize = 25
